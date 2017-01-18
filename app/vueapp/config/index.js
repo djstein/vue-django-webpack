93d8c9558421961949ecf9d7bbff80c07c941332
Dylan Stein <djstein@ncsu.edu>
2017-01-17 19:19:01 -0500

    x

--- app/vueapp/config/index.js
+++ app/vueapp/config/index.js
@@ -5,8 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     // index: path.resolve(__dirname, '../dist/index.html'),
-    // assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsRoot: path.resolve(path.join(__dirname, '../../', '')),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static/',
     assetsPublicPath: '',
     productionSourceMap: true,
